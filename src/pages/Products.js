4674b87694d65ef6e1a06821cf4a91f462d9bf3f
mustaque <hussainmustaque98407@gmail.com>
2021-09-06 23:58:34 +0530

    init

---
+++ src/pages/Products.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+
+const Products = () => {
+  return (
+    <Fragment>
+      <Container>
+        <h1 className="text-center">Products</h1>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default Products;
